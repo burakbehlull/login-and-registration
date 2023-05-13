25286d50539be97749b539463e9f7a36368e88cf
31000s <burakyabgu@gmail.com>
2023-05-13 04:53:25 +0300

    feat: 'user' model created

--- server/base/models.py
+++ server/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    #password?
+    created = models.DateTimeField(auto_now_add=True)
