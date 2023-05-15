7c2dec38bfa5e2e509741339f8fd40e04d606828
31000s <burakyabgu@gmail.com>
2023-05-15 14:38:05 +0300

    feat: User login

--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from base.models import User
-from .serializers import ItemSerializer
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-@api_view(['GET'])
-def getData(request):
-    Users = User.objects.all()
-    datalar = ItemSerializer(Users, many=True)
-    return Response(datalar.data)
+from django.contrib.auth import authenticate
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'username': username})
+        else:
+            return Response({'error': 'Kullanıcı adı veya parola hatalı'})
